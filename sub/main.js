c49bf63325291c0db4c72411028f53f05d941ae6
Nithesh24 <65641597+Nithesh24@users.noreply.github.com>
2024-02-05 10:35:02 +0530

    post option changed for confession

--- sub/main.js
+++ sub/main.js
@@ -9,3 +9,24 @@ toggle.addEventListener("click", (e)=>{
         navContent.style.transform = "translateX(100%)"
 
 })
+
+let post_box = document.querySelector(".post-box");
+post_box.style.display = "none";
+
+function post()
+{
+    console.log(post_box.style.display);
+    
+    if(post_box.style.display == "flex")
+    {
+        post_box.style.animation = "post-box-out 300ms forwards";
+        setTimeout(()=>{post_box.style.display = "none"}, 300)
+
+    }else
+    {
+        post_box.style.animation = "post-animation 300ms forwards";
+        setTimeout(()=>{post_box.style.display = "flex"}, 300)
+    }
+
+
+}
